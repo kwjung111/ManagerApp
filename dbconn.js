a2427b8a22cea45b0ca188d0b2ca8608f02ef7c2
kwjung <kwkwjung@gmail.com>
2023-08-29 20:03:14 +0900

    환경설정

---
+++ dbconn.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2');
+
+
+const dbConnection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DB,
+});
+
+// 연결 시작
+dbConnection.connect(error => {
+  if (error) {
+    console.error('Error connecting to the database:', error);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+module.exports = {
+   dbConnection
+};
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
+require("dotenv").config();
 const http = require('http')
 const WebSocket = require('ws');
+const dbconn = require('./dbconn.js')
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 const port = process.env.PORT || 3000;
-
 
 
 app.use("/", (req, res)=>{
@@ -27,5 +28,8 @@ wss.on('connection', (ws,request) => {
         console.log(`Received message: ${message}`);
     });
 
+    setTimeout(()=>{
+        ws.send('event!!')
+    },5000)
     ws.send('Hello from server!');
 });

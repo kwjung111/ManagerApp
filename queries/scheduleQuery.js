34f5bdd7f03aebf82134cc677440357be6a13bf3
jykang <jykang@businessinsight.co.kr>
2024-01-29 14:31:32 +0900

    개인스케줄 조회조건 수정 + 미 도래 건에 대한 TP 추가 + 최근 3개월 기준 넓힘

--- queries/scheduleQuery.js
+++ queries/scheduleQuery.js
@@ -42,7 +42,12 @@ const scheduleQuery = {
           , M.MTNG_NO                    AS SCHD_NO
           , M.MTNG_PIN_YN                AS SCHD_PIN_YN                     -- 상단 고정 여부
           , M.MTNG_PRTC                  AS SCHD_PRTC                       -- 참가자
-          , CM.CM_ITM_NM                 AS SCHD_PRGSS_CD                   -- 진행 상태
+          , CASE WHEN M.MTNG_STRT_DTM > NOW () THEN '미도래'
+                 ELSE CM.CM_ITM_NM
+            END                          AS SCHD_PRGSS_CD                   -- 진행 상태
+          , CASE WHEN M.MTNG_END_DTM > NOW() THEN 0                         -- 기한에 따른 상태 체크 ( 진행 중 + 기한이 지난 것에 대한 표시 필요 )
+                 ELSE 1
+            END                          AS SCHD_DDLN_OVR                   --          0 : 정상, 1: 미완료+기한 지남
           , ''                           AS SCHD_PRGSS_PRCNT                -- 진척도
           , DATE_FORMAT(M.MTNG_STRT_DTM, '%y년 %m월 %d일 %H시 %i분')    AS SCHD_STRT_DTM    -- (기간) 시작시간
           , DATE_FORMAT(M.MTNG_END_DTM, '%y년 %m월 %d일 %H시 %i분')     AS SCHD_END_DTM    -- (기간) 종료시간
@@ -60,8 +65,8 @@ const scheduleQuery = {
           AND CM.CM_ITM_CD = M.MTNG_PRGSS_CD
           AND MB.MBR_NM = '${data.mbrNm}'
           AND M.MTNG_USE_TF = 1
-          AND M.MTNG_PRGSS_CD != '2'
-          AND M.MTNG_END_DTM > NOW()
+          AND M.MTNG_PRGSS_CD = 1                           -- 진행중인 것만 (완료, 취소 제외)
+--           AND M.MTNG_END_DTM > NOW()
         UNION ALL
         SELECT
             '1'                         AS SCHD_TP
@@ -69,13 +74,18 @@ const scheduleQuery = {
           , P.PRJ_NO                    AS SCHD_NO
           , P.PRJ_PIN_YN                AS SCHD_PIN_YN                     -- 상단 고정 여부
           , P.PRJ_PRTC                  AS SCHD_PRTC                       -- 참가자
-          , CM.CM_ITM_NM                 AS SCHD_PRGSS_CD                   -- 진행 상태
-          , P.PRJ_PRGSS_PRCNT            AS SCHD_PRGSS_PRCNT                -- 진척도
+          , CASE WHEN P.PRJ_STRT_DTM > NOW() THEN '미도래'
+                 ELSE CM.CM_ITM_NM 
+            END                         AS SCHD_PRGSS_CD                   -- 진행 상태
+          , CASE WHEN P.PRJ_END_DTM > NOW() THEN 0                         -- 기한에 따른 상태 체크 ( 진행 중 + 기한이 지난 것에 대한 표시 필요 )
+                 ELSE 1
+            END                         AS SCHD_DDLN_OVR                   --          0 : 정상, 1: 미완료+기한 지남
+          , P.PRJ_PRGSS_PRCNT           AS SCHD_PRGSS_PRCNT                -- 진척도
           , DATE_FORMAT(P.PRJ_STRT_DTM, '%y년 %m월 %d일')   AS SCHD_STRT_DTM    -- (기간) 시작시간
-          , DATE_FORMAT(P.PRJ_END_DTM, '%y년 %m월 %d일')     AS SCHD_END_DTM    -- (기간) 종료시간
+          , DATE_FORMAT(P.PRJ_END_DTM, '%y년 %m월 %d일')    AS SCHD_END_DTM    -- (기간) 종료시간
           , P.PRJ_TOT_TIME              AS SCHD_TOT_TIME                   -- 총 시간(기간)
           , P.PRJ_CNTNTS                AS SCHD_CNTNTS                     -- 내용
-          , DATE_FORMAT(P.PRJ_REG_DTM, '%Y-%m-%d %H:%i')  AS SCHD_REG_DTM  -- 등록 일자
+          , DATE_FORMAT(P.PRJ_REG_DTM, '%Y-%m-%d %H:%i')   AS SCHD_REG_DTM  -- 등록 일자
           , DATE_FORMAT(IFNULL(P.PRJ_MOD_DTM, P.PRJ_REG_DTM), '%Y-%m-%d %H:%i')  AS SCHD_MOD_DTM
           , MB.MBR_NM                                      AS SCHD_WRTR
         FROM PRJ P
@@ -87,8 +97,8 @@ const scheduleQuery = {
           AND CM.CM_ITM_CD = P.PRJ_PRGSS_CD
           AND MB.MBR_NM = '${data.mbrNm}'
           AND P.PRJ_USE_TF = 1
-          AND P.PRJ_PRGSS_CD != '2'
-          AND P.PRJ_END_DTM > NOW()
+          AND P.PRJ_PRGSS_CD = 1                           -- 진행중인 것만 (완료, 취소 제외)
+--           AND P.PRJ_END_DTM > NOW()
         ORDER BY SCHD_PIN_YN DESC, SCHD_STRT_DTM ASC, SCHD_REG_DTM DESC
         `
     },
@@ -119,8 +129,10 @@ const scheduleQuery = {
           AND CM.CM_ITM_CD = M.MTNG_PRGSS_CD
           AND MB.MBR_NM = '${data.mbrNm}'
           AND M.MTNG_USE_TF = 1
-          AND ( M.MTNG_PRGSS_CD = '2'                                                           -- 완료이거나
-                OR M.MTNG_END_DTM BETWEEN DATE_SUB(CURDATE(), INTERVAL 3 MONTH) AND NOW())      -- 종료일자가 3개월 전 ~ 현재 사이에 있는 경우(3개월 전 스케줄이면서, 종료일자가 지금보다 과거일때)
+          AND M.MTNG_PRGSS_CD != 1                                                            -- 진행중이 아니면서 (완료OR취소)
+          AND (M.MTNG_END_DTM BETWEEN DATE_SUB(CURDATE(), INTERVAL 3 MONTH) AND NOW()          -- 기간이 3개월 전 ~ 현재 사이에 있는 경우(3개월 전 스케줄이면서, 시작일자/종료일자가 지금보다 과거일때)
+               OR
+               M.MTNG_STRT_DTM BETWEEN DATE_SUB(CURDATE(), INTERVAL 3 MONTH) AND NOW())
         UNION ALL
         SELECT
             '1'                         AS SCHD_TP
@@ -146,9 +158,11 @@ const scheduleQuery = {
           AND CM.CM_ITM_CD = P.PRJ_PRGSS_CD
           AND MB.MBR_NM = '${data.mbrNm}'
           AND P.PRJ_USE_TF = 1
-          AND ( P.PRJ_PRGSS_CD = '2'
-                OR P.PRJ_END_DTM BETWEEN DATE_SUB(CURDATE(), INTERVAL 3 MONTH) AND NOW())
-        ORDER BY SCHD_PIN_YN DESC, SCHD_STRT_DTM ASC, SCHD_REG_DTM DESC
+          AND P.PRJ_PRGSS_CD != 1                                                             -- 진행중이 아니면서 (완료OR취소)
+          AND (P.PRJ_END_DTM BETWEEN DATE_SUB(CURDATE(), INTERVAL 3 MONTH) AND NOW()           -- 기간이 3개월 전 ~ 현재 사이에 있는 경우(3개월 전 스케줄이면서, 시작일자/종료일자가 지금보다 과거일때)
+               OR
+               P.PRJ_STRT_DTM BETWEEN DATE_SUB(CURDATE(), INTERVAL 3 MONTH) AND NOW())
+        ORDER BY SCHD_PIN_YN DESC, SCHD_END_DTM DESC, SCHD_REG_DTM DESC
         `
     },
 
@@ -156,10 +170,10 @@ const scheduleQuery = {
     getSchdsCount : function(data) {
         return `
         SELECT 
-            COUNT(CASE WHEN A.SCHD_PRGSS_CD != '2' AND A.SCHD_END_DTM > NOW()
+            COUNT(CASE WHEN A.SCHD_PRGSS_CD = 1
                        THEN 1
                   END)          AS ACTING
-            , COUNT(CASE WHEN A.SCHD_PRGSS_CD = '2' OR A.SCHD_END_DTM BETWEEN DATE_SUB(CURDATE(), INTERVAL 3 MONTH) AND NOW()
+            , COUNT(CASE WHEN A.SCHD_PRGSS_CD != 1 AND A.SCHD_END_DTM BETWEEN DATE_SUB(CURDATE(), INTERVAL 3 MONTH) AND NOW()
                     THEN 1
                     END)        AS RECENT
         FROM (
@@ -198,7 +212,7 @@ const scheduleQuery = {
          FROM MTNG
         WHERE 1 = 1
           AND MTNG_USE_TF = 1
-          AND MTNG_PRGSS_CD != '2'
+          AND MTNG_PRGSS_CD = 1
           AND MTNG_END_DTM > NOW()
         UNION ALL
         SELECT
@@ -213,7 +227,7 @@ const scheduleQuery = {
          FROM PRJ
         WHERE 1 = 1
           AND PRJ_USE_TF = 1
-          AND PRJ_PRGSS_CD != 2
+          AND PRJ_PRGSS_CD = 1
           AND PRJ_END_DTM > NOW()
         ORDER BY SCHD_PIN_YN DESC, SCHD_STRT_DTM ASC
         `
